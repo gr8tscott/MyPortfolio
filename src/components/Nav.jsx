b47157503627bf9bdc1dc091aeb9abd73c0c81ad
MatthewScott <mattwillscott95@gmail.com>
2022-09-18 21:04:09 -0400

    project css is well put together

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,29 +3,31 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <nav className="navbar">
-      {/* <h4>kill me
-
-      </h4> */}
-      {/* <div className="navLinks"> */}
-      <div className="nav">
-        <Link to="/">Home</Link>
-      </div>
-      <div className="nav">
-        <Link to="about">About Me</Link>
-      </div>
-      <div className="nav">
-        <Link to="portfolio">Portfolio</Link>
-      </div>
-      <div className="nav">
-        <a
-          className=""
-          href="https://docs.google.com/document/d/1I9Ai_v705heBh9BX-_2KNBRWthvMAe8WEQxYj_dvEaY/edit?usp=sharing"
-        >
-          Resume
+    <nav className="navbar header-container">
+      <div>
+        <a class="h1-container" href="/">
+          <h1>Matthew Scott</h1>
         </a>
       </div>
-      {/* </div> */}
+      <div className="navLinks">
+        <div className="nav">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="nav">
+          <Link to="about">About Me</Link>
+        </div>
+        <div className="nav">
+          <Link to="portfolio">Projects</Link>
+        </div>
+        <div className="nav">
+          <a
+            className=""
+            href="https://docs.google.com/document/d/1I9Ai_v705heBh9BX-_2KNBRWthvMAe8WEQxYj_dvEaY/edit?usp=sharing"
+          >
+            Resume
+          </a>
+        </div>
+      </div>
     </nav>
   )
 }
