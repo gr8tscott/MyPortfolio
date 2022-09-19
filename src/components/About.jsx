22f9a92bd3e0d42ba40c7f3bb42bf492eb9e2b44
MatthewScott <mattwillscott95@gmail.com>
2022-09-19 00:21:11 -0400

    added about section info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,21 +2,55 @@ const About = () => {
   return (
     <div className="about">
       <div>
-        <img src="https://i.imgur.com/2UGx3U7.jpg" className="project-image" />
-        <h1>I was raised on a "work hard, play hard" way of life.</h1>
-        <h2>I always put in the extra hours and effort at the office and </h2>
-        <h2>A few of my hobbies include</h2>
-        <h3>
-          • Traveled to all 7 continents • Esther’s Cookbook -Author,
-          self-published 2020 • Own and operate my own company in the
-          green/renewable energy sector • Scuba-dived over 75+ hours; goal to
-          scuba-dive under the Arctic Ice • Climbed Mt. Fuji & summited 6,
-          14,000+ ft mountains; goal to climb Mt. Kilimanjaro
-        </h3>
-        <img src="https://i.imgur.com/z5XXiCv.jpg" className="project-image" />
-        <h2>Future Goals</h2>
-        <h3>• Own and operate my own company in the green/renewable energy</h3>
-        <h3></h3>
+        <div className="about-header">
+          <h1>I was raised on a "work hard, play hard" way of life.</h1>
+          <h2>
+            I always put in the extra hours and effort at the office and am on
+            the lookout for fun activities to bond with my co-workers.
+          </h2>
+        </div>
+        <div className="project-container">
+          <img
+            src="https://i.imgur.com/2UGx3U7.jpg"
+            className="project-image"
+          />
+          <div>
+            <h2>A few of my personal achievements include</h2>
+            <h3>• Traveled to all 7 continents</h3>
+            <h3>• Esther’s Cookbook -Author, self-published 2020 </h3>
+            <h3>• Scuba-dived over 75+ hours</h3>
+            <h3>• Climbed Mt. Fuji & summited 6, 14,000+ ft mountains</h3>
+          </div>
+        </div>
+        <div className="project-container reverse">
+          <img
+            src="https://i.imgur.com/pV2JoRA.jpg"
+            className="project-image"
+          />
+          <div>
+            <h2>Hobbies</h2>
+            <h3>• Snow-skiing</h3>
+            <h3>• Wakeboarding</h3>
+            <h3>• Sailing</h3>
+            <h3>• Rock Climbing</h3>
+            <h3>• Traveling</h3>
+            <h3>• Gardening</h3>
+          </div>
+        </div>
+        <div className="project-container">
+          <img
+            src="https://i.imgur.com/z5XXiCv.jpg"
+            className="project-image"
+          />
+          <div>
+            <h2>Future Goals</h2>
+            <h3>
+              • Own and operate my own company in the green/renewable energy
+            </h3>
+            <h3>• Climb Mt. Kilimanjaro</h3>
+            <h3>• Scuba-dive under the Arctic Ice</h3>
+          </div>
+        </div>
       </div>
     </div>
   )
